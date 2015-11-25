bdad743f7205a47c972e9cf671a66139ec95fb71
JoyHong <hongwanli163@gmail.com>
2015-11-25 10:59:21 +0800

    Initial Commit

---
+++ BBTReuseBannerScrollViewTests/BBTReuseBannerScrollViewTests.m
@@ -0,0 +1,39 @@
+//
+//  BBTReuseBannerScrollViewTests.m
+//  BBTReuseBannerScrollViewTests
+//
+//  Created by XiaoDou on 15/11/25.
+//  Copyright © 2015年 北京嗨购电子商务有限公司. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BBTReuseBannerScrollViewTests : XCTestCase
+
+@end
+
+@implementation BBTReuseBannerScrollViewTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
